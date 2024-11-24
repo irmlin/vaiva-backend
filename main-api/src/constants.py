46dfde1011d4f8dc8df262f0384c8f80df1909b8
irmantas <irmantas.linkis@agmis.com>
2024-11-24 12:38:14 +0200

    Endpoints for deleting files. Additional validation logic

--- main-api/src/constants.py
+++ main-api/src/constants.py
@@ -2,8 +2,13 @@ import os
 
 STATIC_DIR = 'static/'
 DOCUMENTS_DIR = os.path.join(STATIC_DIR, 'documents')
-AUDIO_DIR = os.path.join(STATIC_DIR, 'audio')
+AUDIO_FOR_AVATAR_DIR = os.path.join(STATIC_DIR, 'audio_for_avatar')
+AUDIO_FOR_VOICE_DIR = os.path.join(STATIC_DIR, 'audio_for_voice')
 VIDEO_DIR = os.path.join(STATIC_DIR, 'video')
 IMAGES_DIR = os.path.join(STATIC_DIR, 'images')
+ALLOWED_DOCUMENTS_EXTENSIONS = ['.doc', '.docx', '.txt', '.xls', '.xlsx', '.csv', '.tsv', 'ppt', 'pptx', '.pdf', '.json', '.md', '.html', '.log']
+ALLOWED_AUDIO_EXTENSIONS = ['.wav', '.mp3']
+ALLOWED_IMAGE_EXTENSIONS = ['.jpg', '.png', '.jpeg']
+ALLOWED_VIDEO_EXTENSIONS = ['.mp4']
 AVATAR_SERVICE_URL = 'http://video-avatar-microservice:8002/generate-video'
 SEND_MESSAGE_SERVICE_URL = 'http://chatbot-microservice:8006/send-message'
---
+++ main-api/src/util/util.py
@@ -0,0 +1,37 @@
+import os
+from typing import List
+
+import aiofiles
+
+from fastapi import HTTPException, UploadFile
+
+
+async def save_input_file_to_storage(file: UploadFile, save_dir: str) -> str:
+    file_path = os.path.join(save_dir, file.filename)
+    if os.path.exists(file_path):
+        raise HTTPException(status_code=400, detail=f"File {file.filename} already exists! Rename the file and try again.")
+    async with aiofiles.open(file_path, 'wb') as out_file:
+        content = await file.read()
+        await out_file.write(content)
+    return file_path
+
+def is_directory_empty(dir: str, extensions: List[str]) -> bool:
+    if not os.path.exists(dir):
+        raise FileNotFoundError(f"The directory {dir} does not exist.")
+
+    for file in os.listdir(dir):
+        if not file.startswith('.') and os.path.isfile(os.path.join(dir, file)):
+            if any(file.lower().endswith(ext) for ext in extensions):
+                return False  # Directory is not empty (has matching file)
+
+    return True  # No matching files found
+
+
+async def delete_file(file_path: str):
+    if not os.path.exists(file_path) or not os.path.isfile(file_path):
+        raise HTTPException(status_code=404, detail=f"File {os.path.basename(file_path)} not found.")
+    try:
+        os.remove(file_path)
+        return {"detail": f"File {os.path.basename(file_path)} has been deleted."}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error deleting file: {str(e)}")
