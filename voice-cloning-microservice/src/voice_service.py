ffc24e636454a8c7231de554f9137991297b6435
irmantas <irmantas.linkis@agmis.com>
2024-12-08 15:04:43 +0200

    Predownload nlp kit, docker volume to maintain generated audio, return audio via endpoint

--- voice-cloning-microservice/src/voice_service.py
+++ voice-cloning-microservice/src/voice_service.py
@@ -21,6 +21,7 @@ class VoiceService:
 
         self.tone_color_converter = ToneColorConverter(f'{ckpt_converter}/config.json', device=self.device)
         self.tone_color_converter.load_ckpt(f'{ckpt_converter}/checkpoint.pth')
+        self.model = TTS(language='EN_NEWEST', device=self.device)
         # Should be loaded before if possible
         
     
@@ -29,31 +30,26 @@ class VoiceService:
         src_path  = f'{self.output_dir}/tmp.wav'
 
         target_se, _ = se_extractor.get_se(audio_path, self.tone_color_converter, target_dir=self.processed_dir, vad=False)
-        texts = {
-            'EN_NEWEST': text
-        }
+        speaker_ids = self.model.hps.data.spk2id
+        for speaker_key in speaker_ids.keys():
+            speaker_id = speaker_ids[speaker_key]
+            speaker_key = speaker_key.lower().replace('_', '-')
 
-        speed = 1
-        for language, text in texts.items():
-            model = TTS(language=language, device=self.device)
-            speaker_ids = model.hps.data.spk2id
+            source_se = torch.load(f'src/checkpoints_v2/base_speakers/ses/{speaker_key}.pth', map_location=self.device)
+            self.model.tts_to_file(text, speaker_id, src_path, speed=1)
+            save_path = f'{self.output_dir}/output_v2_{speaker_key}_{uuid.uuid4()}.wav'
 
-            for speaker_key in speaker_ids.keys():
-                speaker_id = speaker_ids[speaker_key]
-                speaker_key = speaker_key.lower().replace('_', '-')
-                
-                source_se = torch.load(f'src/checkpoints_v2/base_speakers/ses/{speaker_key}.pth', map_location=self.device)
-                model.tts_to_file(text, speaker_id, src_path, speed=speed)
-                save_path = f'{self.output_dir}/output_v2_{speaker_key}.wav'
+            # Run the tone color converter
+            encode_message = "@MyShell"
+            self.tone_color_converter.convert(
+                audio_src_path=src_path,
+                src_se=source_se,
+                tgt_se=target_se,
+                output_path=save_path,
+                message=encode_message)
 
-                # Run the tone color converter
-                encode_message = "@MyShell"
-                self.tone_color_converter.convert(
-                    audio_src_path=src_path, 
-                    src_se=source_se, 
-                    tgt_se=target_se, 
-                    output_path=save_path,
-                    message=encode_message)
+        return FileResponse(path=save_path, filename='speech.wav', media_type="audio/mpeg")
+
     @staticmethod
     async def __save_input_file_to_storage(file: UploadFile, save_dir: str) -> str:
         _, ext = os.path.splitext(file.filename)
